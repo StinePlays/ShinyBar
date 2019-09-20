8478804169c1f7e8eeaa8804b3150150efc57d74
StinePlays <stineplaystv@gmail.com>
2019-09-20 11:22:50 +0200

    changing color of bar with color wheel by Thurallor

--- ShinyBar/BarWindow.lua
+++ ShinyBar/BarWindow.lua
@@ -1,18 +1,24 @@
-BarWindow = class (Turbine.UI.Window)
+--import "Shiny.ShinyBar.ColorPicker.ColorPicker"; TODO
+import "Thurallor.Common.UI.ColorPicker";
+
+BarWindow = class (Turbine.UI.Window);
 
 function BarWindow:Constructor( tSettings )
     self.settings = tSettings
     Turbine.UI.Window.Constructor(self)
     self:SetPosition(self.settings.left,self.settings.top);
     self:SetSize(self.settings.width,self.settings.height);
-    self:SetBackColor(Turbine.UI.Color(self.settings.color.A, self.settings.color.R, self.settings.color.B, self.settings.color.B));
+    self:SetBackColor(Turbine.UI.Color(self.settings.color.A, self.settings.color.R, self.settings.color.G, self.settings.color.B));
     self:SetVisible(self.settings.visibility);
     self.MouseClick = function(sender, args)
         if (args.Button == Turbine.UI.MouseButton.Right) then
             self.Menu:ShowMenu();
         end
     end
+
     self.Menu = Turbine.UI.ContextMenu();
+
+    --New Control Test
     newGeneralControl = Turbine.UI.MenuItem("New GeneralControl test");
     newGeneralControl.Click = function(sender, args)
         control = Settings.GetControlDefaults();
@@ -20,4 +26,18 @@ function BarWindow:Constructor( tSettings )
         control:SetText("Test");
     end
     self.Menu:GetItems():Add(newGeneralControl);
+
+    --Change background color of bar
+    changeBackgroundColor = Turbine.UI.MenuItem("Change background color");
+    changeBackgroundColor.Click = function(sender, args)
+        bgColor = Thurallor.UI.ColorPicker(self:GetBackColor());
+        
+        bgColor.ColorChanged = function(picker)
+            local color = picker:GetColor();
+            self:SetBackColor(color);
+            self.settings.color = color;
+            Settings.Save();
+        end
+    end
+    self.Menu:GetItems():Add(changeBackgroundColor);
 end
